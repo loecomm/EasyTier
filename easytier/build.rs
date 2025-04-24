f8c578766eb62a03de9b8bf93a8c232dfca85af0
loecomm <loecom@qq.com>
2025-04-24 12:43:45 +0800

    add thunk-rs to support win7

--- easytier/build.rs
+++ easytier/build.rs
@@ -129,7 +129,7 @@ fn check_locale() {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	// enable thunk-rs when target os is windows and arch is i686
 	#[cfg(target_os = "windows")]
-    if std::env::var("TARGET").unwrap_or_default().contains("i686"){
+    if !std::env::var("TARGET").unwrap_or_default().contains("aarch64"){
         thunk::thunk();
     }
 
