fcd0bef8b78be77af5993da71efa99766c567ee0
loecomm <loecom@qq.com>
2025-04-24 11:24:35 +0800

    fix thunk-rs to support win7

--- easytier-gui/src-tauri/build.rs
+++ easytier-gui/src-tauri/build.rs
@@ -1,8 +1,6 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows"
-        && std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default() == "msvc"
-        && std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default() == "x86"
-    {
+	let target = std::env::var("TARGET").unwrap();
+    if target == "x86_64-pc-windows-msvc" {
         thunk::thunk();
     }
 
--- easytier/build.rs
+++ easytier/build.rs
@@ -128,10 +128,8 @@ fn check_locale() {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // enable thun when target os is windows and arch is x86_64 or i686
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows"
-        && std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default() == "msvc"
-        && std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default() == "x86"
-    {
+	let target = std::env::var("TARGET").unwrap();
+    if target == "x86_64-pc-windows-msvc" {
         thunk::thunk();
     }
 
