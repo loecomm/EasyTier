15d8e2ff1b6c47bc64514621fcdc68eb88c519ac
loecomm <loecom@qq.com>
2025-04-24 11:56:10 +0800

    add thunk-rs to support win7

--- easytier-gui/src-tauri/build.rs
+++ easytier-gui/src-tauri/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+	// enable thunk-rs when target os is windows and arch is i686
 	#[cfg(target_os = "windows")]
     if env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "i686" {
         thunk::thunk();
--- easytier/build.rs
+++ easytier/build.rs
@@ -127,7 +127,7 @@ fn check_locale() {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // enable thun when target os is windows and arch is x86_64 or i686
+    // enable thunk-rs when target os is windows and arch is i686
 	#[cfg(target_os = "windows")]
     if env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "i686" {
         thunk::thunk();
